492e007d72e13516fddc659c27b3e00f54953936
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-16 19:59:15 +0100

    Create a new Test flow

--- IncomeInsightEngine/IncomeInsightEngine/src/IncomeInsightEngineMain.cs
+++ IncomeInsightEngine/IncomeInsightEngine/src/IncomeInsightEngineMain.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Security.Principal;
 using dataStructure;
 using IncomeInsightEngine.src.dataStructure.management;
+
 
 namespace src
 {
@@ -9,50 +11,23 @@ namespace src
     {
         public static void Main(string[] args)
         {
-            Transaction transaction = new Transaction
-            {
-                Id = 1,
-                Date = DateTime.Now,
-                Amount = -50.75m,
-                Category = "Lebensmittel",
-                Description = "Wochenendeinkauf",
-                PaymentMethod = "Debitkarte",
-                Frequency = "einmalig",
-                Location = "Berlin, Deutschland",
-                Account = "Girokonto",
-                Tags = new List<string> { "Wochenende", "Lebensmittel" },
-                Partner = "Supermarkt XY",
-                Priority = "notwendig",
-                Status = "abgeschlossen",
-                Project = "Familienbudget",
-                TaxDeductible = false,
-                Currency = "EUR",
-                Reimbursable = false,
-                Receipt = "path/to/receipt1.jpg",
-                BudgetCategory = "Alltag",
-                Notes = "Sonderangebote genutzt",
-                Classification = "manuell"
-            };
 
-            // Anzeige der Transaktionsdetails
-            transaction.DisplayTransactionDetails();
+            TransactionManager manager = new TransactionManager();
 
-            // Erstelle eine Instanz der JsonTransaction Klasse
-            JsonTransaction jsonTransaction = new JsonTransaction();
 
-            // Erstelle die JSON-Datei, wenn sie nicht existiert
-            jsonTransaction.CreateFile();
+            Console.WriteLine("Id");
+            manager.DisplayAllTransactionsShortInComandline();
 
-            // Füge die neue Transaktion zur JSON-Datei hinzu
-            bool addResult = jsonTransaction.AddTransaction(transaction);
-            Console.WriteLine($"Transaction added: {addResult}");
+            manager.SortTransactionsByAmountAscending();
 
-            // Lese alle Transaktionen aus der JSON-Datei
-            List<Transaction> transactions = jsonTransaction.ReadData();
-            foreach (var trans in transactions)
-            {
-                trans.DisplayTransactionDetails();
-            }
+            Console.WriteLine("Amount Ascending");
+            manager.DisplayAllTransactionsShortInComandline();
+
+            manager.SortTransactionsByDateAscending();
+
+            Console.WriteLine("Date Ascending");
+            manager.DisplayAllTransactionsShortInComandline();
+
         }
     }
 }
