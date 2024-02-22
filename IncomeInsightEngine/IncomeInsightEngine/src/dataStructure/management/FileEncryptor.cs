f2668e2a24b547d8ae9b093cae8b9b4a2aa517f9
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-22 23:24:32 +0100

    Enhance security for long time transaction file saving

--- IncomeInsightEngine/IncomeInsightEngine/src/dataStructure/management/FileEncryptor.cs
+++ IncomeInsightEngine/IncomeInsightEngine/src/dataStructure/management/FileEncryptor.cs
@@ -3,20 +3,61 @@ using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 
+
 namespace IncomeInsightEngine.src.dataStructure.management
 {
     public class FileEncryptor
     {
-        // Die Methoden benötigen einen Schlüssel und einen Initialisierungsvektor (IV).
-        // Diese sollten sicher gespeichert und gehandhabt werden.
-        private readonly byte[] Key = Encoding.UTF8.GetBytes("6cuamzua12kmp3xy"); // Sollte 16 Bytes (128 Bit) sein
-        private readonly byte[] IV = Encoding.UTF8.GetBytes("5618395375629671"); // Sollte 16 Bytes (128 Bit) sein
+        private readonly string keyFilePath = "encryptionKey.bin";
+        private readonly string ivFilePath = "encryptionIV.bin";
 
-        public void EncryptFile(string filePath, string dataToEncrypt)
+        public FileEncryptor()
         {
+
+            string keyFilePathappDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            keyFilePath = Path.Combine(keyFilePathappDataPath, "IncomeInsightEngine", "encryptionKey.bin");
+
+            string ivFilePathappDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            ivFilePath = Path.Combine(ivFilePathappDataPath, "IncomeInsightEngine", "encryptionIV.bin");
+
+            InitializeEncryptionKeys();
+        }
+
+        private void InitializeEncryptionKeys()
+        {
+            if (!File.Exists(keyFilePath) || !File.Exists(ivFilePath))
+            {
+                using (Aes aesAlg = Aes.Create())
+                {
+                    ProtectAndSave(aesAlg.Key, keyFilePath);
+                    ProtectAndSave(aesAlg.IV, ivFilePath);
+                }
+            }
+        }
+
+        private void ProtectAndSave(byte[] data, string filePath)
+        {
+            // Verschlüsselt die Daten mit DPAPI und speichert sie in der Datei.
+            byte[] protectedData = ProtectedData.Protect(data, null, DataProtectionScope.CurrentUser);
+            File.WriteAllBytes(filePath, protectedData);
+        }
+
+        private byte[] UnprotectAndLoad(string filePath)
+        {
+            // Lädt die Daten aus der Datei und entschlüsselt sie mit DPAPI.
+            byte[] protectedData = File.ReadAllBytes(filePath);
+            return ProtectedData.Unprotect(protectedData, null, DataProtectionScope.CurrentUser);
+        }
+
+        public void EncryptFile(string filePath)
+        {
+            string dataToEncrypt = File.ReadAllText(filePath);
+            byte[] key = UnprotectAndLoad(keyFilePath);
+            byte[] IV = UnprotectAndLoad(ivFilePath);
+
             using (Aes aesAlg = Aes.Create())
             {
-                aesAlg.Key = Key;
+                aesAlg.Key = key;
                 aesAlg.IV = IV;
 
                 ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
@@ -31,14 +72,17 @@ namespace IncomeInsightEngine.src.dataStructure.management
         }
 
         public string DecryptFile(string filePath)
-        {
+        {        
+            byte[] key = UnprotectAndLoad(keyFilePath);
+            byte[] IV = UnprotectAndLoad(ivFilePath);
 
-            string data = File.ReadAllText(filePath);
+            string decryptedData = File.ReadAllText(filePath);
+           
             try
             {
                 using (Aes aesAlg = Aes.Create())
                 {
-                    aesAlg.Key = Key;
+                    aesAlg.Key = key;
                     aesAlg.IV = IV;
 
                     ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
@@ -47,23 +91,24 @@ namespace IncomeInsightEngine.src.dataStructure.management
                     using (CryptoStream cryptoStream = new CryptoStream(fileStream, decryptor, CryptoStreamMode.Read))
                     using (StreamReader decryptReader = new StreamReader(cryptoStream))
                     {
-                        return decryptReader.ReadToEnd();
+                        decryptedData = decryptReader.ReadToEnd();
+                                          
                     }
                 }
             }
             catch (CryptographicException ex)
             {
-                
+
                 Console.WriteLine("A cryptographic error occurred: " + ex.Message);
-               
+
             }
             catch (Exception ex)
             {
-                
+
                 Console.WriteLine("An error occurred: " + ex.Message);
             }
-            File.WriteAllText(filePath, data);
-            return data; 
+            File.WriteAllText(filePath, decryptedData);
+            return decryptedData;
         }
     }
 }
