fba5dc02135ebb7592176b8c00c39c8b76469ad1
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-22 23:20:06 +0100

    Change test case so it uses the example csv

--- IncomeInsightEngine/IncomeInsightEngine/src/IncomeInsightEngineMain.cs
+++ IncomeInsightEngine/IncomeInsightEngine/src/IncomeInsightEngineMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Security.Principal;
 using dataStructure;
 using IncomeInsightEngine.src.dataStructure.management;
+using src.parser;
 
 
 namespace src
@@ -12,13 +13,22 @@ namespace src
         public static void Main(string[] args)
         {
 
-            TransactionManager manager = new TransactionManager();         
+
+            
+            TransactionManager manager = new TransactionManager();                                           
+           
+            
+            DKBParser parser = new DKBParser();
+
+            List<Transaction> t =   parser.ParseCsv("Link to file");
+
+           
+            manager.AddTransaction(t);
+
             manager.DisplayAllTransactionsShortInComandline();
 
-            manager.Close();
+       
             
-
-
         }
     }
 }
