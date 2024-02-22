af9f44bcc5472de62851b1762a50ccdfee01b601
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-22 23:22:56 +0100

    Create the parser for the csv file format from the DKB

--- IncomeInsightEngine/IncomeInsightEngine/src/parser/DKBParser.cs
+++ IncomeInsightEngine/IncomeInsightEngine/src/parser/DKBParser.cs
@@ -1,5 +1,8 @@
+using dataStructure;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +10,38 @@ namespace src.parser
 {
     public class DKBParser
     {
-        
+        public List<Transaction> ParseCsv(string filePath)
+        {
+            var transactions = new List<Transaction>();
+            var lines = File.ReadAllLines(filePath).Skip(5); 
+
+            foreach (var line in lines)
+            {
+                var cleanedLine = line.Replace("\"", "");
+
+                var columns = cleanedLine.Split(';');
+               
+                bool eingang = false;
+                if (columns[6].Equals("Eingang")) {
+                    eingang = true;
+                }
+
+                var transaction = new Transaction
+                {
+                    Date = DateTime.ParseExact(columns[0], "dd.MM.yy", CultureInfo.InvariantCulture),
+                    Status = columns[2],
+                    Partner = (eingang) ? columns[3] : columns[4],
+                    PartnerIBAN = columns[7],
+                    Description = columns[5],
+                    Amount = decimal.Parse(columns[8].Replace(",", "."), CultureInfo.InvariantCulture),
+                    Notes = columns[11],
+                };
+
+                transactions.Add(transaction);
+
+            }
+
+            return transactions;
+        }
     }
 }
