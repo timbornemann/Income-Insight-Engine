ee171357a04867959a8cc6fbb505e6de3c3b2b87
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-23 19:53:59 +0100

    Add Window for adding a manual tranaction to the system

---
+++ IncomeInsightEngine/IncomeInsightEngine/src/ui/externalFrames/ManualTransactionEntryWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IncomeInsightEngine.src.ui.externalFrames
+{
+    internal class ManualTransactionEntryWindow : Window
+    {
+
+        public ManualTransactionEntryWindow()
+        {
+            
+            this.Title = "Manual Transaction Entry";
+            this.Width = 800;
+            this.Height = 600;
+        }
+
+
+
+    }
+}
