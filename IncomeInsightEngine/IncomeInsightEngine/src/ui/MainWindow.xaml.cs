dfd0897a08dbf5f932565d88bcdf381123adb3c8
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-28 18:19:38 +0100

    Start working on the main window

--- IncomeInsightEngine/IncomeInsightEngine/src/ui/MainWindow.xaml.cs
+++ IncomeInsightEngine/IncomeInsightEngine/src/ui/MainWindow.xaml.cs
@@ -3,17 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using IncomeInsightEngine.Properties;
 using System.Windows;
+using System.Windows.Input;
+using IncomeInsightEngine.src.dataStructure.management;
+using IncomeInsightEngine.src.ui.UserControls;
+
 
 namespace IncomeInsightEngine.src.ui
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        public TransactionManager Manager { get; }
+
+
+        public MainWindow(TransactionManager manager)
         {
             InitializeComponent();
+
+            Manager = manager;
+
+            this.overview.AddElementToOverview(new SingleTransaction());
+            this.overview.AddElementToOverview(new SingleTransaction());
+            this.overview.AddElementToOverview(new SingleTransaction());
+            this.overview.AddElementToOverview(new SingleTransaction());
+            this.overview.AddElementToOverview(new SingleTransaction());
         }
+
     }
+
 
 }
