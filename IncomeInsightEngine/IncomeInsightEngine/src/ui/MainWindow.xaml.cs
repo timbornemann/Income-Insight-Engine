52904630f933393989e89ed32acc458b6334630e
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-02-28 15:49:45 +0100

    Add main window for the application

---
+++ IncomeInsightEngine/IncomeInsightEngine/src/ui/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows;
+
+namespace IncomeInsightEngine.src.ui
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
