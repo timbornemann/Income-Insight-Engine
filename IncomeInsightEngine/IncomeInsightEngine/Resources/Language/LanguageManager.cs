b960e0d4d3da900a0f446af179ccfb27dccf776e
Tim Bornemann <timbornemann@users.noreply.github.com>
2024-03-17 17:14:04 +0100

    Create Language manager for changing languages

---
+++ IncomeInsightEngine/IncomeInsightEngine/Resources/Language/LanguageManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace IncomeInsightEngine.Resources.Language
+{
+    public class LanguageManager
+    {
+        public enum Language
+        {
+            English,
+            German,        
+        }
+
+
+       public static void SetLanguage(Language language)
+        {
+            switch (language)
+            {
+                case Language.English:
+                    SetCulture("en-US");
+                    break;
+                case Language.German:
+                    SetCulture("de-DE");
+                    break;
+            }
+        }
+
+
+
+      private static void SetCulture(string cultureCode)
+        {
+            CultureInfo culture = new CultureInfo(cultureCode);
+
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+        }
+
+
+    }
+}
